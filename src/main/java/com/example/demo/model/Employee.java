a1e028b4e3b84afe0e3e77f7c4fa336bec453fdf
bala-amypo <bala@amypo.in>
2025-12-17 09:47:31 +0000

    User commit

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,101 +1,32 @@
-package com.example.demo.modul;
+package com.example.demo.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-
+import jakarta.persistence.Column;
 
 @Entity
-public class Employee {
+public class Employee{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String fullName;
-
-    @Column(nullable = false, unique = true)
+    @Column(unique=true)
     private String email;
-
     private String department;
-
     private String jobTitle;
+    private Boolean active;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 
-    @Column(nullable = false)
-    private Boolean active = true;
-
-    @Column(updatable = false)
-    private Instant createdAt;
-
-    private Instant updatedAt;
-
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = Instant.now();
-        this.updatedAt = Instant.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = Instant.now();
-    }
-
-
-    public Long getId() {
+    public long getId() {
         return id;
     }
-
-    public String getFullName() {
-        return fullName;
+    public void setId(Long id){
+        this.id=id;
     }
+    public String
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
 }
