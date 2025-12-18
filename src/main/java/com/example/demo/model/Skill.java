c71a3016e9014260d56fe22efb059c99080e930e
bala-amypo <bala@amypo.in>
2025-12-18 05:18:08 +0000

    User commit

--- src/main/java/com/example/demo/model/Skill.java
+++ src/main/java/com/example/demo/model/Skill.java
@@ -1,11 +1,14 @@
 package com.example.demo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
 
 @Entity
 public class Skill{
     @Id
     private Long id;
-    @
+    @Column(unique=true)
+    private String 
 
 }
