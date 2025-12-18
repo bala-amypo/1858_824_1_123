b91fa0cae18c3f3fab81bdb516c906276aca47db
bala-amypo <bala@amypo.in>
2025-12-18 05:53:00 +0000

    User commit

--- src/main/java/com/example/demo/model/Skill.java
+++ src/main/java/com/example/demo/model/Skill.java
@@ -27,6 +27,15 @@ public class Skill{
     public void setName(String name){
         this.name=name;
     }
-    public String getCategory
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category){
+        this.category=category;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescri
 
 }
