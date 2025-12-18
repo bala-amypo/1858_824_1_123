bca3598c76806098567188c25709a9c19725fe68
bala-amypo <bala@amypo.in>
2025-12-18 10:13:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillRepository.java
+++ src/main/java/com/example/demo/repository/SkillRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.SkillCategory;
+import com.example.demo.model.Skilly;
 
-public interface Skill extends JpaRepository<SkillCategory,Long>{
+public interface Skill extends JpaRepository<Skill,Long>{
 
 }
