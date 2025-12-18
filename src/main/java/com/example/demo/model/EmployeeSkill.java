29a081d5ce119b55a5253777c37f943d13ea4dac
bala-amypo <bala@amypo.in>
2025-12-18 08:38:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/EmployeeSkill.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class EmployeeSkill{
+    @Id
+    private Long id;
+    private String 
+}
