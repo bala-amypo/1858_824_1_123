0a18d04e927ac9b90e553df165d757e69eecd815
bala-amypo <bala@amypo.in>
2025-12-18 06:38:00 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillCategory.java
+++ src/main/java/com/example/demo/model/SkillCategory.java
@@ -8,5 +8,15 @@ import jakarta.persistence.Id;
 public class SkillCategory{
     @Id
     private Long id;
-    
+    @column(unique = true)
+    private String categoryName;
+    private String description;
+    private Booleaan active;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
