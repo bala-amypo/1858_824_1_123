37c8700aca0f3c736ee10550bb795cf98e6ee5ee
bala-amypo <bala@amypo.in>
2025-12-18 10:03:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.;
+import com.example.demo.model.Employee;
 
-public interface InteractionRuleRepository extends JpaRepository<InteractionRule,Long>{
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
 
 }
---
+++ src/main/java/com/example/demo/repository/SearchQueryRecordRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.EmployeeSkill;
+
+public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill,Long>{
+
+}
