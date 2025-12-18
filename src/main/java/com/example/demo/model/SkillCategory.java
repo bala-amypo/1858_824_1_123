5c252845f8af9db67c438c695a2e98c12a190044
bala-amypo <bala@amypo.in>
2025-12-18 08:28:12 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillCategory.java
+++ src/main/java/com/example/demo/model/SkillCategory.java
@@ -1,60 +1,50 @@
 package com.example.demo.model;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 
-@Entity
-public class SkillCategory {
 
+
+@Entity
+public class SkillCategory{
     @Id
     private Long id;
-
     @Column(unique = true)
     private String categoryName;
-
     private String description;
-
     private Boolean active;
-
-    public SkillCategory() {}
-
-    public SkillCategory(Long id, String categoryName, String description, Boolean active) {
-        this.id = id;
-        this.categoryName = categoryName;
-        this.description = description;
-        this.active = active;
-    }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getCategoryName() {
         return categoryName;
     }
-
     public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
+        this.categoryName=categoryName;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
-        this.description = description;
+        this.description=description;
     }
-
     public Boolean getActive() {
         return active;
     }
-
     public void setActive(Boolean active) {
-        this.active = active;
+        this.active=active;
+    }
+    public SkillCategory(Long id,String categoryName,String description,Boolean active){
+        this.id=id;
+        this.categoryName=categoryName;
+        this.description=description;
+        this.active=active;
+    }
+    public SkillCategory(){
+
     }
 }
