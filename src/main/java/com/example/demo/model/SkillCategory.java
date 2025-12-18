f0692f2827c706462a167c100600e1dcf2e1297b
bala-amypo <bala@amypo.in>
2025-12-18 06:33:01 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillCategory.java
+++ src/main/java/com/example/demo/model/SkillCategory.java
@@ -1,4 +1,12 @@
 package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 
 
 @Entity
+public class SkillCategory{
+    @Id
+    private Long id;
+    
+}
