94fb4ab6abfd61a1d0dcb8cfef1c50a2517991a9
bala-amypo <bala@amypo.in>
2025-12-21 13:41:25 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SkillServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/SkillServiceimpl.java
@@ -5,15 +5,18 @@ import com.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.repository.SkillRepository;
 import jakarta.stereotype.Service;
 import java.util.List;
-import com.springframework.http.
+import com.springframework.http.ResponseEntity;
 
 @Service
 public class SkillServiceimpl implements SkillService{
-
+    @Autowired
+    SkillRepository obj;
     Skill createSkill(Skill skill){
+        return obj.save(skill);
+    }
+    Skill updateSkill(Long id,Skill skill){
         
     }
-    Skill updateSkill(Long id,Skill skill);
     Skill getSkillById(Long id);
     List<Skill> getAllSkills();
     ResponseEntity<String> deactivateSkill(Long id);
