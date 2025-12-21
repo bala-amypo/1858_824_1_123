6089c8cd37e536a6a9d496f45769117f58e9a440
bala-amypo <bala@amypo.in>
2025-12-21 17:25:25 +0000

    User commit

--- src/main/java/com/example/demo/service/SearchQueryService.java
+++ src/main/java/com/example/demo/service/SearchQueryService.java
@@ -1,14 +1,14 @@
 package com.example.demo.service;
 
 import java.util.List;
-import com.example.demo.model.SearchQuery;
+import com.example.demo.model.SearchQueryRecord;
 import org.springframework.http.ResponseEntity;
 
 public interface SearchQueryService {
 
-    SearchQuery createSearchQuery(SearchQuery obj);
-    SearchQuery updateSearchQuery(Long id, SearchQuery obj);
-    SearchQuery getSearchQueryById(Long id);
-    List<SearchQuery> getAllSearchQuery();
+    SearchQueryRecord createSearchQuery(SearchQueryRecord obj);
+    SearchQueryRecord updateSearchQuery(Long id, SearchQueryRecord obj);
+    SearchQueryRecord getSearchQueryById(Long id);
+    List<SearchQueryRecord> getAllSearchQuery();
     ResponseEntity<String> deactivateSearchQuery(Long id);
 }
