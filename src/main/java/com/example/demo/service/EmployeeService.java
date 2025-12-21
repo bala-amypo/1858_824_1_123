cb0abe5ed072aeee0ca28578a95616a094a8f6dc
bala-amypo <bala@amypo.in>
2025-12-21 13:53:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Employee;
+import java.util.List;
+import org.springframework.ResponseEntity;
+public interface SkillService{
+    Skill createSkill(Skill skill);
+    Skill updateSkill(Long id,Skill skill);
+    Skill getSkillById(Long id);
+    List<Skill> getAllSkills();
+    ResponseEntity<String> deactivateSkill(Long id);
+}
