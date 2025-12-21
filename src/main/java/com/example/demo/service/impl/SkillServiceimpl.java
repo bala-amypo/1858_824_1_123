ad51c2d2d8c2b28f23749fbab0ba31daa034b80c
bala-amypo <bala@amypo.in>
2025-12-21 13:44:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SkillServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/SkillServiceimpl.java
@@ -1,23 +1,67 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.Skill;
 import com.example.demo.service.SkillService;
-import com.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.example.demo.repository.SkillRepository;
-import jakarta.stereotype.Service;
+
 import java.util.List;
-import com.springframework.http.ResponseEntity;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Service
-public class SkillServiceimpl implements SkillService{
+public class SkillServiceImpl implements SkillService{
+
     @Autowired
     SkillRepository obj;
-    Skill createSkill(Skill skill){
+
+    @Override
+    public Skill createSkill(Skill skill){
         return obj.save(skill);
     }
-    Skill updateSkill(Long id,Skill skill){
-        
+
+    @Override
+    public Skill updateSkill(Long id, Skill skill){
+        Optional<Skill> existing = obj.findById(id);
+
+        if(existing.isPresent()){
+            Skill update = existing.get();
+            update.setSkillName(skill.getSkillName());
+            update.setDescription(skill.getDescription());
+            update.setActive(skill.getActive());
+
+            return obj.save(update);
+        }
+        return null; 
     }
-    Skill getSkillById(Long id);
-    List<Skill> getAllSkills();
-    ResponseEntity<String> deactivateSkill(Long id);
+    @Override
+    public Skill getSkillById(Long id){
+        return obj.findById(id)
+                  .orElse(null);
+    }
+
+    @Override
+    public List<Skill> getAllSkills(){
+        return obj.findAll();
+    }
+
+    @Override
+    public ResponseEntity<String> deactivateSkill(Long id){
+
+        Optional<Skill> skill = obj.findById(id);
+
+        if(skill.isPresent()){
+            Skill temp = skill.get();
+            temp.setActive(false);
+            obj.save(temp);
+
+            return ResponseEntity.ok("Skill Deactivated Successfully");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body("Skill Not Found");
+    }
 }
