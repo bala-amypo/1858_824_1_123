8bc32b8a4ad19a091cc71850fd477a63d14f56a2
bala-amypo <bala@amypo.in>
2025-12-21 13:50:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SkillServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/SkillServiceimpl.java
@@ -17,51 +17,7 @@ public class SkillServiceImpl implements SkillService{
 
     @Autowired
     SkillRepository obj;
+    
+ 
 
-    @Override
-    public Skill createSkill(Skill skill){
-        return obj.save(skill);
-    }
-
-    @Override
-    public Skill updateSkill(Long id, Skill skill){
-        Optional<Skill> existing = obj.findById(id);
-
-        if(existing.isPresent()){
-            Skill update = existing.get();
-            update.setSkillName(skill.getSkillName());
-            update.setDescription(skill.getDescription());
-            update.setActive(skill.getActive());
-
-            return obj.save(update);
-        }
-        return null; 
-    }
-    @Override
-    public Skill getSkillById(Long id){
-        return obj.findById(id)
-                  .orElse(null);
-    }
-
-    @Override
-    public List<Skill> getAllSkills(){
-        return obj.findAll();
-    }
-
-    @Override
-    public ResponseEntity<String> deactivateSkill(Long id){
-
-        Optional<Skill> skill = obj.findById(id);
-
-        if(skill.isPresent()){
-            Skill temp = skill.get();
-            temp.setActive(false);
-            obj.save(temp);
-
-            return ResponseEntity.ok("Skill Deactivated Successfully");
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                             .body("Skill Not Found");
-    }
 }
