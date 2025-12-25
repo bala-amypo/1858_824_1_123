79656ee925591c598e0262919a77893e6026c759
bala-amypo <bala@amypo.in>
2025-12-25 09:13:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
@@ -1,17 +1,13 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Employee;
 import com.example.demo.model.EmployeeSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
-    List<Employee> findEmployeesByAllSkillNames(
-            @Param("skills") List<String> skills,
-            @Param("skillCount") Long skillCount
-    );
+
+    List<EmployeeSkill> findBySkill_NameInIgnoreCaseAndActiveTrue(List<String> skillNames);
 
     List<EmployeeSkill> findByEmployeeIdAndActiveTrue(Long employeeId);
 
