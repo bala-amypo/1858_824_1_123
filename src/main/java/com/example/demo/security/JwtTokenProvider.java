84a9dad583d3e803217178b7772bb3c4177ff3cd
bala-amypo <bala@amypo.in>
2025-12-25 09:34:49 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,10 +1,13 @@
 package com.example.demo.security;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Component
 public class JwtTokenProvider {
 
     private final Map<String, Map<String, Object>> tokenStore = new HashMap<>();
