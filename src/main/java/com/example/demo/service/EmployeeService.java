e2baaab82c5efaf860f732f453b8e0aa977eb4b9
bala-amypo <bala@amypo.in>
2025-12-25 07:10:49 +0000

    User commit

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -2,12 +2,16 @@ package com.example.demo.service;
 
 import com.example.demo.model.Employee;
 import java.util.List;
-import org.springframework.http.ResponseEntity;
 
 public interface EmployeeService {
-    public Employee createEmployee(Employee employee);
-    public Employee updateEmployee(Long id, Employee employee);
-    public Employee getEmployeeById(Long id);
-    public List<Employee> getAllEmployees();
-    public ResponseEntity<String> deactivateEmployee(Long id);
+
+    Employee createEmployee(Employee employee);
+
+    Employee updateEmployee(Long id, Employee employee);
+
+    Employee getEmployeeById(Long id);
+
+    List<Employee> getAllEmployees();
+
+    void deactivateEmployee(Long id);
 }
