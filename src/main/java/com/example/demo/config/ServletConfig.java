daf095eefb07ef8c9b73edb68aaa28ba0706c5b6
bala-amypo <bala@amypo.in>
2025-12-25 08:37:49 +0000

    User commit

--- src/main/java/com/example/demo/config/ServletConfig.java
+++ src/main/java/com/example/demo/config/ServletConfig.java
@@ -1,3 +1,32 @@
-public class ServletConfig{
-    
+package com.example.demo.config;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class ServletConfig {
+
+    @Bean
+    public ServletRegistrationBean<HttpServlet> helloServlet() {
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(
+                    HttpServletRequest request,
+                    HttpServletResponse response
+            ) throws IOException {
+
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().write("Hello from simple servlet");
+            }
+        };
+
+        return new ServletRegistrationBean<>(servlet, "/hello-servlet");
+    }
 }
