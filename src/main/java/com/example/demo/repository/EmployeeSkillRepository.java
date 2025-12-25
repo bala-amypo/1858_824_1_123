ad5e9ffa20569e146f89b92c8bd79f6d9a336ff7
bala-amypo <bala@amypo.in>
2025-12-25 09:10:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeSkillRepository.java
@@ -3,21 +3,11 @@ package com.example.demo.repository;
 import com.example.demo.model.Employee;
 import com.example.demo.model.EmployeeSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
-
-    @Query(""" SELECT es.employee
-           FROM EmployeeSkill es
-           WHERE LOWER(es.skill.name) IN :skills
-           AND es.employee.active = true
-           AND es.active = true
-           GROUP BY es.employee
-           HAVING COUNT(DISTINCT LOWER(es.skill.name)) = :skillCount
-           """)
     List<Employee> findEmployeesByAllSkillNames(
             @Param("skills") List<String> skills,
             @Param("skillCount") Long skillCount
